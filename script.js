5edd930ccc5b402f3b5bf733bf1eed1984f9187e
Magishetty Saikumar <saimagishetty01@gmail.com>
2023-11-20 18:54:43 +0530

    update

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-$('.portfolio-button').mousedown(function(){
-    timeout = setInterval(function(){
-        window.scrollBy(0,-1); // May need to be -1 to go down
+
+var div = document.getElementById('viewAllSkills');
+div.style.display = 'none';
+var btn = document.getElementById('toggleButton2');
+btn.style.display = 'none';
+$('.portfolio-button').mousedown(function () {
+    timeout = setInterval(function () {
+        window.scrollBy(0, -1); // May need to be -1 to go down
     }, 0); // Play around with this number. May go too fast
 
     return false;
 });
+function openClose(e) {
+    if (e == 1) {
+        var div = document.getElementById('viewAllSkills');
+        div.style.display = 'block';
+        var btn1 = document.getElementById('toggleButton1');
+        btn1.style.display = 'none';
+        var btn2 = document.getElementById('toggleButton2');
+        btn2.style.display = 'block';
+    }
+    else if(e==2){
+        var div = document.getElementById('viewAllSkills');
+        div.style.display = 'none';
+        var btn1 = document.getElementById('toggleButton1');
+        btn1.style.display = 'block';
+        var btn2 = document.getElementById('toggleButton2');
+        btn2.style.display = 'none';
+    }
+}
